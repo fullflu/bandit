a643c13ccfa06d05b175dfce96d07d95a0d5f1d9
koin <k.takayama0902@gmail.com>
2017-05-05 10:25:43 +0900

    add bound and visualize function

--- README.md
+++ README.md
@@ -1,21 +1,30 @@
 # Bandits
 implement famous bandit algorithms
 
+## Requirements
+
+I checked algorithms in the following environment:
+
+* OS X(10.9+), CPU
+* python (2.7.11)
+* numpy (1.11.3)
+* matplotlib (1.4.3)
+
 ## K-armed Bernoulli Bandits
 
 * Epsilon-greedy
-* UCB1
+* UCB
 * KL-UCB
 * Thompson sampling
 
-Simple test result is: 
-
-<img src="https://github.com/fullflu/bandit/blob/master/figures/regret_bernoulli.png">
-
+Simple test can be done as follows:
 ```
 python models.py
 ```
 
+The result is shown as follows: 
+
+<img src="https://github.com/fullflu/bandit/blob/master/figures/regret_bernoulli.png">
 
 ## Contextual Bandits (incomplete)
 
@@ -24,3 +33,6 @@ python models.py
 2: Epsilon greedy action selection with online logistic regression
 
 3: Random action selection with online logistic regression (for comparison)
+
+
+
--- models.py
+++ models.py
@@ -15,7 +15,7 @@ class BernoulliBanditBase(object):
         self.total_trial = 0
         self.history = []
 
-    def ucb1(self):
+    def ucb(self):
         return self.params + np.sqrt(np.log(self.total_trial) / (2*self.trials) )
 
     def kl(self, p, q):
@@ -60,7 +60,7 @@ class BernoulliBanditBase(object):
         self.total_trial += 1
 
 class BernoulliBandit(BernoulliBanditBase):
-    def __init__(self, arm_num, strategy, eps_greedy = 0.1, c = 0, delta = 1e-12, eps = 1e-8, max_iter = 1e3
+    def __init__(self, arm_num, strategy, eps_greedy = 0.01, c = 0, delta = 1e-12, eps = 1e-8, max_iter = 1e3
         , prior_alpha = 0.5, prior_beta = 0.5):
         super(BernoulliBandit, self).__init__(arm_num)
         self.strategy = strategy
@@ -83,9 +83,9 @@ class BernoulliBandit(BernoulliBanditBase):
             else:
                 return np.argmax(self.params)
 
-        elif self.strategy =="ucb1":
-            ucb1_results = self.ucb1()
-            return np.argmax(ucb1_results)
+        elif self.strategy =="ucb":
+            ucb_results = self.ucb()
+            return np.argmax(ucb_results)
 
         elif self.strategy == "kl_ucb":
             klucb_results = self.kl_ucb(self.c, self.delta, self.eps, self.max_iter)
@@ -109,7 +109,7 @@ class BernoulliBandit(BernoulliBanditBase):
         print(self.strategy + "params ={}".format(self.params))
 
 
-def tmp(arm):
+def tmp_reward(arm):
     # temporary reward function
     return np.random.binomial(1, true_params[arm])
 
@@ -120,22 +120,53 @@ def get_regret():
     regrets = best_reward - expected_rewards
     return regrets
 
+def get_asymptoic_lower_bound(mode):
+    if mode == "simple":
+        return [0.1*(arm_num - 1)/agent.kl(0.4,0.5) * (np.log(t) - np.log(100)) for t in range(int(T))]
+    else:
+        return [sum([(true_params[0] - true_params[k]) * (np.log(t) - np.log(100))/ (agent.kl(true_params[k], true_params[0]) ) for k in range(1,arm_num)]) for t in range(int(T))]
 
+def visualize_regret(mode):
+    max_num = 0
+    for strategy in strategy_list:
+        cum_regrets = np.load("results/regret_" + strategy + ".npy")
+        plt.plot(cum_regrets, label = strategy)
+        max_num = max(max_num, max(cum_regrets))
+    #asymptoic_lower_bounds = [sum([(true_params[0] - true_params[k]) * (np.log(t) - np.log(100))/ (kl(true_params[k], true_params[0]) ) for k in range(1,arm_num)]) for t in range(1, int(T))]
+    #ab=[0.1*99/kl(0.4,0.5) * (np.log(t) - np.log(100)) for t in range(int(T))]
+    ab = get_asymptoic_lower_bound(mode)
+    max_num = max(max(ab), max_num)
+    plt.plot(ab, label = "asymptoic bound")
+    #plt.xticks([1e2,1e3,1e4,1e5], ["1e2", "1e3", "1e4", "1e5"])
+    xticks_name = ["1e" + str(t) for t in range(2, int(np.log10(T)))]
+    xticks = [float(xtick) for xtick in xticks_name]
+    plt.xticks(xticks, xticks_name)
+    plt.xlim(xmin = 100)
+    #plt.ylim(0,10000)
+    plt.ylim(0,max_num*2)
+    plt.xscale("log")
+    plt.legend(loc="best")
+    #plt.show()
+    plt.savefig("figures/regret_bernoulli.png")
 
 # simple model test
-arm_num = 3
-true_params = np.random.uniform(0.05, 0.2 ,size = arm_num)
-#true_params = np.arange(arm_num) / np.arange(arm_num)
-strategy_list = ["egreedy", "ucb1", "ts","kl_ucb"]
+T = 1e4
+arm_num = 10
+#true_params = np.random.uniform(0.05, 0.2 ,size = arm_num)
+#true_params = 0.1 + np.arange(arm_num) / (arm_num * 5.0)
+true_params = np.ones(arm_num) * 0.4
+true_params[0] += 0.1
+strategy_list = ["egreedy", "ucb", "ts","kl_ucb"]
+#eps_greedy_optimal = (2.0 * arm_num * np.log(T) ) / ( 0.005 * T)
 
 print("true_params = {}".format(true_params))
 
 for strategy in strategy_list:
     agent = BernoulliBandit( arm_num, strategy)
     t = 0
-    while t < 1e5:
+    while t < T:
         arm = agent.select_arm()
-        reward = tmp(arm)
+        reward = tmp_reward(arm)
         agent.save_history(arm, reward)
         agent.update_arm()
         t += 1
@@ -144,9 +175,8 @@ for strategy in strategy_list:
     regrets = get_regret()
     cum_regrets = np.cumsum(regrets)
     np.save("results/regret_" + strategy , cum_regrets)
-    plt.plot(cum_regrets, label = strategy)
-plt.legend(loc="best")
-plt.savefig("figures/regret_bernoulli.png")
+    #plt.plot(cum_regrets, label = strategy)
 
+visualize_regret("simple")
 
 
